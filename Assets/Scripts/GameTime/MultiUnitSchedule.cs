da26b57dbdf40f78e467b30f89ef0e7112827f59
Denny Scott <dennyscott301@gmail.com>
2016-04-10 19:24:54 -0500

    Added base time

---
+++ Assets/Scripts/GameTime/MultiUnitSchedule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameTime
+{
+    public class MultiUnitSchedule : ISchedule
+    {
+        private List<int> _scheduledUnits;
+        private int _flushDate;
+
+        public MultiUnitSchedule(List<int> units)
+        {
+            _flushDate = units[0];
+            for (var i = 0; i < units.Count; i++)
+            {
+                if (units[i] > _flushDate)
+                {
+                    _flushDate = units[i];
+                }
+            }
+
+            _scheduledUnits = units;
+        }
+
+        public bool HasUnit(int unit)
+        {
+            return _scheduledUnits.Exists(e => e == unit);
+        }
+
+        public bool IsFlushDate(int unit)
+        {
+            return unit == _flushDate;
+        }
+    }
+}
---
+++ Assets/Scripts/GameTime/SingleUnitSchedule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameTime
+{
+    public class SingleUnitSchedule : ISchedule
+    {
+        public SingleUnitSchedule(int unit)
+        {
+            _scheduledUnit = unit;
+        }
+
+        private int _scheduledUnit;
+
+        public bool HasUnit(int unit)
+        {
+            return _scheduledUnit == unit;
+        }
+
+        public bool IsFlushDate(int unit)
+        {
+            return unit == _scheduledUnit;
+        }
+    }
+}
