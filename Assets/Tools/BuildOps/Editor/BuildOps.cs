93cd68be94a3b2cb10b891d529a1a80527d505e3
Denny Scott <dennyscott301@gmail.com>
2016-04-20 00:16:49 -0500

    comment for linux build

--- Assets/Tools/BuildOps/Editor/BuildOps.cs
+++ Assets/Tools/BuildOps/Editor/BuildOps.cs
@@ -4,6 +4,8 @@ using UnityEditor;
 
 public class BuildOps
 {
+    //xvfb-run --server-args="-screen 0 1024x768x24" /opt/Unity/Editor/Unity -batchmode -logfile -force-opengl - quit -projectPath /var/lib/jenkins/workspace/ThemeDev/ -executeMethod BuildOps.PerformWindowsBuild
+
     static string[] SCENES = FindEnabledEditorScenes();
 
     static string APP_NAME = "ThemeDev";
