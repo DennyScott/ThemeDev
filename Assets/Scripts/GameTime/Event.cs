98283bddea87f0b124dd2d1003cf12575558b040
Travis Scott <travisscott301@gmail.com>
2016-04-24 23:25:16 -0500

    Updated Docs and Efficiency.

--- Assets/Scripts/GameTime/Event.cs
+++ Assets/Scripts/GameTime/Event.cs
@@ -1,43 +1,92 @@
 ﻿using System.Collections.Generic;
-using UnityEngine;
 
 namespace GameTime
 {
+    /// <summary>
+    /// Abstract class event, used in the GameTime event loop.
+    /// </summary>
     public abstract class Event : IEvent
     {
+        #region Private Fields
+        private ISchedule _scheduledUnit;
+        #endregion
+
+        #region Contructors
+
+        /// <summary>
+        /// Constructor for event. This constuctor creates a single scheduled date. Schedules the event at the date depending on the manualDate.
+        /// </summary>
+        /// <param name="unit">The unit to schedule this event for.</param>
+        /// <param name="manualDate">if true, will schedule the event for the passed day, and if false, schedules it for the amount of units from current day</param>
         protected Event(int unit, bool manualDate = false)
         {
             unit = CalculateUnit(unit, manualDate); 
             _scheduledUnit = new SingleUnitSchedule(unit);
         }
 
+        /// <summary>
+        /// Constructor for event. This constuctor creates multiple scheduled dates. Schedules the events at the date depending on the manualDate.
+        /// </summary>
+        /// <param name="units">A list of scheduled dates</param>
+        /// <param name="manualDate">if true, will schedule the dates for the passed day, and if false, schedules it for the amount of units from current day</param>
         protected Event(List<int> units, bool manualDate = false)
         {
+            // For each unit in the units list...
             for (var i = 0; i < units.Count; i++)
+                // ...Calculate the date of the unit, and place it back into the same position of the units list.
                 units[i] = CalculateUnit(units[i], manualDate);
 
+            // Creates a MultiUnitSchedule object with the updates units list.
             _scheduledUnit = new MultiUnitSchedule(units);
         }
 
-        private ISchedule _scheduledUnit;
+        #endregion
 
-        public virtual void TriggerEvent()
+        #region Abstract Methods
+
+        /// <summary>
+        /// Abstract method, triggered when this object hits its scheduled date(s).
+        /// </summary>
+        public abstract void TriggerEvent();
+
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Checks to see if the passed unit is a scheduled date of the event
+        /// </summary>
+        /// <param name="unit">The unit to check is a scheduled date of this event</param>
+        /// <returns>true if the unit is a scheduled date of this event, false otherwise</returns>
+        public bool IsScheduledDate(int unit)
         {
+            return _scheduledUnit.IsScheduledDate(unit);
         }
 
-        public bool IsScheduled(int unit)
-        {
-            return _scheduledUnit.HasUnit(unit);
-        }
-
-        public bool IsFlushedUnit(int unit)
+        /// <summary>
+        /// Checks to see if the passed unit is the flushed date of this event
+        /// </summary>
+        /// <param name="unit">The unit to check is the flush date</param>
+        /// <returns>True is the unit is the flush date, false otherwise</returns>
+        public bool IsFlushedDate(int unit)
         {
             return _scheduledUnit.IsFlushDate(unit);
         }
 
+        #endregion
+
+
+        #region Private Methods
+        /// <summary>
+        /// Calculates the scheduled unit based on the manual date.
+        /// </summary>
+        /// <param name="unit">The unit to schedule for the scheduled date</param>
+        /// <param name="manualDate">If true, the unit passed will be the scheduled date, if false, it will be the amount of units ahead of the current date</param>
+        /// <returns>The unit that will be the scheduled date</returns>
         private int CalculateUnit(int unit, bool manualDate)
         {
             return manualDate ? unit : GameTime.CurrentUnit + unit;
         }
+
+        #endregion
     }
 }
--- Assets/Scripts/GameTime/MultiUnitSchedule.cs
+++ Assets/Scripts/GameTime/MultiUnitSchedule.cs
@@ -1,36 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace GameTime
 {
+    /// <summary>
+    /// Contains multuple scheduled dates for an event.  Will be flushed on the latest date.
+    /// </summary>
     public class MultiUnitSchedule : ISchedule
     {
+
+        #region Private Fields
+        // List of scheduled units
         private List<int> _scheduledUnits;
+
+        // The flush date of this object
         private int _flushDate;
 
+        #endregion
+
+        #region Constructor
         public MultiUnitSchedule(List<int> units)
         {
-            _flushDate = units[0];
-            for (var i = 0; i < units.Count; i++)
-            {
-                if (units[i] > _flushDate)
-                {
-                    _flushDate = units[i];
-                }
-            }
+            // If units is passed as null...
+            if (units == null)
+                // ...then throw a new argument null exception.
+                throw new System.ArgumentNullException("units", "Passed into a MultiUnitSchedule constructor as null");
 
+            // Gets the latest date in the units list as the flush date
+            _flushDate = GetLatestDate(units);
+
+            // scheduledunits is the passed unit list
             _scheduledUnits = units;
         }
 
-        public bool HasUnit(int unit)
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Check to see if the passed unit is a scheduled date in the list of dates for this object
+        /// </summary>
+        /// <param name="unit">The unit to check is a unit in the scheduled units</param>
+        /// <returns>True if the unit is a date in the scheduled units list</returns>
+        public bool IsScheduledDate(int unit)
         {
-            return _scheduledUnits.Exists(e => e == unit);
+            return _scheduledUnits.Contains(unit);
         }
 
+        /// <summary>
+        /// Checks to see if the passed unit is the flush date of this object
+        /// </summary>
+        /// <param name="unit">The unit to check is the flush date unit</param>
+        /// <returns>True if the flush date matches the passed unit</returns>
         public bool IsFlushDate(int unit)
         {
             return unit == _flushDate;
         }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Gets the latest day from the list of units
+        /// </summary>
+        /// <param name="units">A list of units</param>
+        /// <returns>The latest unit in the list of int units</returns>
+        private int GetLatestDate(List<int> units) {
+            // If units is passed as null...
+            if (units == null)
+                // ...then throw a new argument null exception.
+                throw new System.ArgumentNullException("units", "Passed into a MultiUnitSchedule constructor as null");
+
+            // Gets the 0th list item as a base case.
+            var latestDate = units[0];
+
+            // For each item in the units list...
+            for (var i = 0; i < units.Count; i++) {
+                // ...If the unit at the ith position is a larger value then the latest date variable...
+                if (units[i] > latestDate) {
+                    // ...Then set the latest date variable to that value.
+                    latestDate = units[i];
+                }
+            }
+
+            //Return the latest date
+            return latestDate;
+        }
+
+        #endregion
     }
 }
