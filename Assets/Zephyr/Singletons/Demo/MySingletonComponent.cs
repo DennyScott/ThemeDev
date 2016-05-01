08a39c02b33ed137b267d1776828e3b56127036e
Travis Scott <travisscott301@gmail.com>
2016-05-01 18:03:02 -0500

    Added AdvancedMonoBehaviour into the project, and added a game runner to handle the update loop

---
+++ Assets/Zephyr/Singletons/Demo/MySingletonComponent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Zephyr.Singletons;
+
+public class MySingletonComponent : SingletonAsComponent<MySingletonComponent> {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
