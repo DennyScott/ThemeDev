3d99ad8ff565c952e33c3778e57968b503f6fbf7
Denny Scott <dennyscott301@gmail.com>
2016-04-30 23:00:07 -0500

    Added inital tests. Moved EventManager to model to allow for additional testing

---
+++ Assets/Zephyr/EventSystem/Core/GameEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Zephyr.EventSystem.Core
+{
+  public class GameEvent
+  {
+    public string Name;
+    public GameEvent()
+    {
+      Name = this.GetType().Name;
+    }
+  }
+}
